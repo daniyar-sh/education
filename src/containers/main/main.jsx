d762ea3a88c2a7751b86ee80ee9b442f69abb303
Daniyar-shakirat <daniyar.shakirat@gmail.com>
2022-07-17 17:58:35 +0600

    additionally

---
+++ src/containers/main/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Main = () => {
+    return (
+        <>
+        <Link to='/artwork'>artwork</Link>
+        <Link to='/blogs'>blog</Link>
+        </>
+        
+    )
+}
+export default Main
